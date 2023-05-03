dc61392ea1480fc00208783a5f2311446adec643
Justin Worley <jkworley@gmail.com>
2023-05-03 12:52:46 -0400

    completed lab - unsure about code

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+
+function superbowlWin(arr) {
+    let findWin = arr.find(item => item.result === "W");
+    if (typeof findWin === 'object') {
+        return findWin.year;
+    } else {
+        return findWin;
+    }
+}
